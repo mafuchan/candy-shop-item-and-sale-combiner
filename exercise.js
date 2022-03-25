0a3b7655f631cbce9b26cc33defa7f53ace45c99
Matt Crecraft <mcrecraft@gmail.com>
2022-03-25 12:36:20 -0500

    First function completed

--- exercise.js
+++ exercise.js
@@ -1,8 +1,29 @@
 // Your code goes here
+const items = [{
+    id: 1,
+    description: "Dark Chocolate Crunchies",
+    price: 4.29
+},{
+    id: 2,
+    description: "Mint Wafers",
+    price: 1.09
+},{
+    id: 3,
+    description: "Peppermint Poppers",
+    price: 2.38
+}];
+
+
+
+
+function getItemById(items, id){
+    return items.filter(item => item.id == id)[0];
+}
+
 
 module.exports = {
     // Uncomment these as you write them
-    // getItemById,
+    getItemById,
     // buildTransactions,
     // getTransactionsByItemDescription
 }
