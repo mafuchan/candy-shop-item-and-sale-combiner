bad7be9725dfae5cc3cc5483a950a971758e65d4
Matt Crecraft <mcrecraft@gmail.com>
2022-04-01 14:30:12 -0500

    Completed assignment and removed arrays

--- exercise.js
+++ exercise.js
@@ -1,47 +1,31 @@
 // Your code goes here
-const items = [{
-    id: 1,
-    description: "Dark Chocolate Crunchies",
-    price: 4.29
-},{
-    id: 2,
-    description: "Mint Wafers",
-    price: 1.09
-},{
-    id: 3,
-    description: "Peppermint Poppers",
-    price: 2.38
-}];
-
-const sales = [{
-    itemId: 1,
-    quantity: 2
-},{
-    itemId: 2,
-    quantity: 2
-},{
-    itemId: 1,
-    quantity: 2
-},{
-    itemId: 3,
-    quantity: 3
-}];
-
-
-function getItemById(items, id){
-    return items.filter(item => item.id == id)[0];
+function getItemById(items, itemId) {
+    return items.find(item => item.id === itemId)
 }
 
-function buildTransactions(sales, items){
+function killId(object) {
+    delete object.id
+    return object
+}
+function buildTransactions(sales, items) {
     return sales.map(sale => {
-        return Object.assign(sale, getItemById(items, sale.itemId));
-})}
+        return {
+            ...sale,
+            ...killId(
+                getItemById(items, sale.itemId)
+            ),
+        }
+    })
+}
 
+function getTransactionsByItemDescription(transactions, itemDescription) {
+    return transactions.filter(transaction => transaction.description === itemDescription)
+}
 
 
 module.exports = {
     // Uncomment these as you write them
     getItemById,
     buildTransactions,
-    // getTransactionsByItemDescription
+    getTransactionsByItemDescription,
 }
